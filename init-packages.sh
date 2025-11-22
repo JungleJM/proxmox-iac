8bc2411995f52a074ba395611caee72196530e87
Jungle J(i)M <jasonvmathias@outlook.com>
2025-11-22 04:04:41 -0600

    Refactor Docker installation in init-packages.sh
    Updated Docker installation process to use new GPG key and repository format.

--- init-packages.sh
+++ init-packages.sh
@@ -12,14 +12,25 @@ apt upgrade -y
 echo "Installing dependencies and git..."
 apt install -y apt-transport-https ca-certificates curl gnupg lsb-release git
 
-echo "Adding Docker's official GPG key and Debian repository..."
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
-https://download.docker.com/linux/debian bookworm stable" > /etc/apt/sources.list.d/docker.list
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
 echo "Installing Docker and docker-compose..."
 apt update -y
-apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Installing Podman and podman-compose..."
 apt install -y podman podman-compose
